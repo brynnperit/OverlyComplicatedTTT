4770e66867197f2c0c1fa2c029c65d5eed9a81d5
brynnperit <brynnperit@gmail.com>
2023-11-01 14:03:57 -0400

    Initial autogenerated code from start.spring.io

---
+++ src/main/java/com/brynnperit/OverlyComplicatedTTT/OverlyComplicatedTttApplication.java
@@ -0,0 +1,13 @@
+package com.brynnperit.OverlyComplicatedTTT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverlyComplicatedTttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OverlyComplicatedTttApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brynnperit/OverlyComplicatedTTT/OverlyComplicatedTttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brynnperit.OverlyComplicatedTTT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverlyComplicatedTttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
